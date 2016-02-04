3490b9ab04ac63e62a62dd1c5cba603dd273ad62
Sean Morris <smorris@library.rochester.edu>
2016-02-04 15:02:52 -0500

    Added second sidebar back

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -132,7 +132,7 @@
            <li><a href="#">Separated link</a></li> -->
         </li>
         </ul>
-        <li><a href="http://livinghistory.lib.rochester.edu">About Project</a></li>
+        <li><a href="http://livinghistory.lib.rochester.edu">About</a></li>
         <!-- Nav Search box -->
         <li>
           <div class="header-search">
