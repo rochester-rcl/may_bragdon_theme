de6329dcb3721999a874f3c203844df142907731
Sean Morris <smorris@library.rochester.edu>
2016-02-09 15:38:19 -0500

    Need sidebar code

--- templates/page--type-diary--object.tpl.php
+++ templates/page--type-diary--object.tpl.php
@@ -215,7 +215,7 @@
 
 
     <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
+      <aside class="inclusion-sidebar" role="complementary">
         <?php print render($page['sidebar_second']); ?>
       </aside>  <!-- /#sidebar-second -->
     <?php endif; ?>
