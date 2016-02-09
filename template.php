d605cf37a78edc512216e3fbb80e0994abc01fb1
nate-rcl <nsarr@library.rochester.edu>
2016-02-09 12:08:28 -0500

    target specific pages

--- template.php
+++ template.php
@@ -5,53 +5,90 @@
  */
 function may_bragdon_theme_preprocess_page(&$vars) {
 
-  // Do we have a node?
-  if (isset($vars['node'])) {
+    // Do we have a node?
+    if (isset($vars['node'])) {
+        // Ref suggestions.
+        $suggests = &$vars['theme_hook_suggestions'];
 
-    // Ref suggestions.
-    $suggests = &$vars['theme_hook_suggestions'];
+        // Get path arguments.
+        $args = arg();
+        // Remove first argument of "node".
+        unset($args[0]);
 
-    // Get path arguments.
-    $args = arg();
-    // Remove first argument of "node".
-    unset($args[0]);
+        // Set type.
+        $type = "page__type_{$vars['node']->type}";
 
-    // Set type.
-    $type = "page__type_{$vars['node']->type}";
+        // Bring it all together.
+        $suggests = array_merge(
+                $suggests, array($type), theme_get_suggestions($args, $type)
+        );
 
-    // Bring it all together.
-    $suggests = array_merge(
-      $suggests,
-      array($type),
-      theme_get_suggestions($args, $type)
-    );
-
-    // if the url is: 'http://domain.com/node/123/edit'
-    // and node type is 'blog'..
-    //
+        // if the url is: 'http://domain.com/node/123/edit'
+        // and node type is 'blog'..
+        //
     // This will be the suggestions:
-    //
+        //
     // - page__node
-    // - page__node__%
-    // - page__node__123
-    // - page__node__edit
-    // - page__type_blog
-    // - page__type_blog__%
-    // - page__type_blog__123
-    // - page__type_blog__edit
-    //
+        // - page__node__%
+        // - page__node__123
+        // - page__node__edit
+        // - page__type_blog
+        // - page__type_blog__%
+        // - page__type_blog__123
+        // - page__type_blog__edit
+        //
     // Which connects to these templates:
-    //
+        //
     // - page--node.tpl.php
-    // - page--node--%.tpl.php
-    // - page--node--123.tpl.php
-    // - page--node--edit.tpl.php
-    // - page--type-blog.tpl.php
-    // - page--type-blog--%.tpl.php
-    // - page--type-blog--123.tpl.php
-    // - page--type-blog--edit.tpl.php
-    //
+        // - page--node--%.tpl.php
+        // - page--node--123.tpl.php
+        // - page--node--edit.tpl.php
+        // - page--type-blog.tpl.php
+        // - page--type-blog--%.tpl.php
+        // - page--type-blog--123.tpl.php
+        // - page--type-blog--edit.tpl.php
+        //
     // Latter items take precedence.
-  }
+    } else {
+        if (isset($vars['page']['content']['system_main']['islandora_book'])) {
+            
+            // Ref suggestions.
+            $suggests = &$vars['theme_hook_suggestions'];
 
+            // Get path arguments.
+            $args = arg();
+            // Remove first argument of "node".
+            unset($args[0]);
+
+            // Set type.
+            $type = "page__type_diary";
+
+            // Bring it all together.
+            $suggests = array_merge(
+                    $suggests, array($type), theme_get_suggestions($args, $type)
+            );
+            
+        } else if (isset($vars['page']['content']['islandora_compound_object_compound_navigation'])) {
+            // Ref suggestions.
+            $suggests = &$vars['theme_hook_suggestions'];
+
+            // Get path arguments.
+            $args = arg();
+            // Remove first argument of "node".
+            unset($args[0]);
+
+            // Set type.
+            $type = "page__type_inclusion";
+
+            // Bring it all together.
+            $suggests = array_merge(
+                    $suggests, array($type), theme_get_suggestions($args, $type)
+            );
+        } else {
+            //dpm("don't know");
+        }
+    }
+
+    dpm("vars");
+    dpm($vars);
 }
---
+++ templates/page--type-inclusion--object.tpl.php
@@ -0,0 +1,290 @@
+<?php
+/**
+ * @file
+ * Default theme implementation to display a single Drupal page.
+ *
+ * The doctype, html, head and body tags are not in this template. Instead they
+ * can be found in the html.tpl.php template in this directory.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - $base_path: The base URL path of the Drupal installation. At the very
+ *   least, this will always default to /.
+ * - $directory: The directory the template is located in, e.g. modules/system
+ *   or themes/bartik.
+ * - $is_front: TRUE if the current page is the front page.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
+ *
+ * Site identity:
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
+ *   when linking to the front page. This includes the language domain or
+ *   prefix.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $site_name: The name of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
+ *   in theme settings.
+ *
+ * Navigation:
+ * - $main_menu (array): An array containing the Main menu links for the
+ *   site, if they have been configured.
+ * - $secondary_menu (array): An array containing the Secondary menu links for
+ *   the site, if they have been configured.
+ * - $breadcrumb: The breadcrumb trail for the current page.
+ *
+ * Page content (in order of occurrence in the default page.tpl.php):
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title: The page title, for use in the actual HTML content.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ * - $messages: HTML for status and error messages. Should be displayed
+ *   prominently.
+ * - $tabs (array): Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node).
+ * - $action_links (array): Actions local to the page, such as 'Add menu' on the
+ *   menu administration interface.
+ * - $feed_icons: A string of all feed icons for the current page.
+ * - $node: The node object, if there is an automatically-loaded node
+ *   associated with the page, and the node ID is the second argument
+ *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - $page['help']: Dynamic help text, mostly for admin pages.
+ * - $page['highlighted']: Items for the highlighted content region.
+ * - $page['content']: The main content of the current page.
+ * - $page['sidebar_first']: Items for the first sidebar.
+ * - $page['sidebar_second']: Items for the second sidebar.
+ * - $page['header']: Items for the header region.
+ * - $page['footer']: Items for the footer region.
+ *
+ * @see bootstrap_preprocess_page()
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ * @see bootstrap_process_page()
+ * @see template_process()
+ * @see html.tpl.php
+ *
+ * @ingroup themeable
+ */
+?>
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+
+<!--====== UNIVERSITY HEADER ======-->
+<div class="uofr-header">
+  <div class="container">
+    <!-- UofR Container -->
+    <div class="uofr-logo-container">
+      <a href="http://www.rochester.edu/">
+        <img class="navbar-brand-uofr-logo" alt="University of Rochester" src="<?php print base_path() . drupal_get_path('theme', 'may_bragdon_theme');?>/images/logo-uofr-rcl.png" />
+      </a>
+    </div>
+    <!-- Ever Better Container -->
+    <div class="everbetter-container">
+        <img class="navbar-brand-everbetter" alt="University of Rochester" src="<?php print base_path() . drupal_get_path('theme', 'may_bragdon_theme');?>/images/logo-dp.png" />
+    </div>
+  </div>
+</div>
+
+
+<!--======  HEADER ======-->
+<div class="inside-nav">
+<header id="navbar" role="banner" class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <!-- LHP Logo -->
+      <a class="navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><img class="lhp-logo" alt="May Bragdon" src="<?php print base_path() . drupal_get_path('theme', 'may_bragdon_theme');?>/images/mayb-logo.png" />
+      </a>
+      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <!-- Nav links -->
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Diaries <span class="caret"></span></a>
+         <ul class="dropdown-menu">
+           <li><a href="https://rclinddev.lib.rochester.edu/maybragdon/islandora/object/maybragdon%3A1">February 27-December 31, 1893</a></li>
+           <!-- <li><a href="#">April 6, 1895-February 26, 1896</a></li>
+           <li><a href="#">February 29, 1896-February 24, 1897</a></li> -->
+           <li><a href="https://rclinddev.lib.rochester.edu/maybragdon/islandora/object/maybragdon%3A576">February 25, 1897-November 11, 1898</a></li>
+           <!-- <li><a href="#">November 12, 1898-January 18, 1902</a></li>
+           <li><a href="#">January 19, 1902-February 4, 1904</a></li>
+           <li><a href="#">February 26, 1904-June 8, 1905</a></li>
+           <li><a href="#">June 9, 1905-October 17, 1907</a></li>
+           <li><a href="#">October 18, 1907-December 31, 1909</a></li>
+           <li><a href="#">January 1, 1910-December 31, 1914</a></li> -->
+
+           <!-- <li role="separator" class="divider"></li>
+           <li><a href="#">Separated link</a></li> -->
+        </li>
+        </ul>
+        <li><a href="http://livinghistory.lib.rochester.edu">About</a></li>
+        <!-- Nav Search box -->
+        <li>
+          <div class="header-search">
+            <form class="navbar-form navbar-left" name="sitesearch" action="http://www.library.rochester.edu/site-search" method="get">
+                <div class="form-group">
+                  <input class="form-control navbar-search-grow" type="text" name="search"  placeholder="Search" title="Seach the Library website">
+                </div>
+              </form>
+              <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search nav-search-icon" aria-hidden="true"></span></button>
+            </form>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</header>
+</div>
+<div class="spacer"></div>
+<!-- /Header/ -->
+
+
+
+<!--======= /HEADER  ========-->
+
+
+
+
+
+
+<div class="main-container container">
+
+  <header role="banner" id="page-header">
+    <?php if (!empty($site_slogan)): ?>
+      <p class="lead">Site slogan</p>
+    <?php endif; ?>
+    <?php print render($page['header']); ?>
+  </header> <!-- /#page-header -->
+
+  <div class="row">
+    <?php if (!empty($page['sidebar_first'])): ?>
+      <aside class="col-sm-3" role="complementary">
+        <?php print render($page['sidebar_first']); ?>
+      </aside>  <!-- /#sidebar-first -->
+    <?php endif; ?>
+
+    <section<?php print $content_column_class; ?>>
+      <?php if (!empty($page['highlighted'])): ?>
+        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+      <?php endif; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+
+<!--===== TABS =====-->
+    <?php if (!empty($tabs)): ?>
+      <div class="tab-container-edit-screens">
+      <?php print render($tabs); ?>
+    </div>
+    <?php endif; ?>
+
+
+<!-- Page title  -->
+      <?php if (!empty($title)): ?>
+        <div class="page-title-wrapper">
+          <h1 class="page-header basic-page-title"><?php print $title; ?></h1>
+          <h1>Inclusion Page</h1>
+        </div>
+      <?php endif; ?>
+
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+
+      <?php if (!empty($page['help'])): ?>
+        <?php print render($page['help']); ?>
+      <?php endif; ?>
+      <?php if (!empty($action_links)): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+
+      <div class="content-h-c">
+      <?php print render($page['content']); ?>
+    </div>
+
+
+    </section>
+
+    <?php if (!empty($page['sidebar_second'])): ?>
+      <aside class="col-sm-3" role="complementary">
+        <?php print render($page['sidebar_second']); ?>
+      </aside>  <!-- /#sidebar-second -->
+    <?php endif; ?>
+
+  </div>
+</div>
+
+
+<!--====== FOOTER ======-->
+<div class="footer" style="margin-top: 100px;">
+    <div class="container">
+      <!-- Right Side of Footer -->
+        <div class="footer-right">
+          <div class="address">
+            <p>
+              Rare Books Special Collections Preservation <br>
+              Rush Rhees Library <br>
+              Second Floor, Room 225 <br>
+              755 Library Road <br>
+              University of Rochester	<br>
+              Rochester, NY 14627-0055 <br>
+            </p>
+            <p>
+              Phone: (585) 275-4477 <br>
+              Fax: (585) 273-1032 <br>
+              <a href="mailto:rarebks@library.rochester.edu">rarebks@library.rochester.edu</a>
+            </p>
+          </div>
+        </div>
+
+      <!-- Left Side of Footer -->
+      <div class="footer-left">
+
+        <!-- <div class="social-icons">
+          <a href="https://www.facebook.com/rivercampuslibraries"><i class="fa fa-facebook-square"></i></a>
+          <a href="https://twitter.com/rclibraries"><i class="fa fa-twitter-square"></i></a>
+          <a href="https://www.youtube.com/user/RCLibraries"><i class="fa fa-youtube-square"></i></a>
+          <a href="https://www-flickr-com.pc181.lib.rochester.edu/photos/carlsonlibrary/"><i class="fa fa-flickr"></i></a>
+        </div> -->
+        <ul class="">
+          <li><a href="#">Advanced Search</a></li>
+          <li></li>
+          <li></li>
+        </ul>
+
+        <!-- Copyright  -->
+        <div class="copyright">Copyright © 1998-2015. All Rights Reserved.<br>
+          University of Rochester | River Campus Libraries <br>
+          Rare Books, Special Collections and Preservation
+          <a href="http://www.rochester.edu/">
+            <img class="footer-uofr-logo" alt="University of Rochester" src="<?php print base_path() . drupal_get_path('theme', 'may_bragdon_theme');?>/images/logo-uofr.png" />
+          </a>
+        </div>
+
+        <?php if (!empty($page['footer'])): ?>
+            <?php print render($page['footer']); ?>
+        <?php endif; ?>
+
+      </div>
+
+
+
+
+</div>
+
+
+<!--====== Navigation Scroll JS ======-->
+<script src="<?php print base_path() . drupal_get_path('theme', 'may_bragdon_theme') . '/js/nav.js'; ?>"></script>
