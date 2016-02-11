36007a0c0022bfd9016874da5caeb8eb1c73f5e4
Adam Traub <greenlibrarian@gmail.com>
2016-02-11 15:51:46 -0500

    Bringing in islandora search with MayB classes

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -59,13 +59,16 @@
         <!-- Nav Search box -->
         <li>
           <div class="header-search">
-            <form class="navbar-form navbar-left" name="maybsearch" id="islandora-solr-simple-search-form" action="islandora/search/" method="post" accept-charset="UTF-8">
-              <div class="form-group form-item form-type-textfield form-item-islandora-simple-search-query" id="edit-simple">
-                <input class="form-control navbar-search-grow" type="text" name="islandora_simple_search_query"  placeholder="Search" title="Seach the May Bragdon Diaries">
-                <input type="hidden" name="type" value="dismax">
-                <input type="hidden" name="form_id" value="islandora_solr_simple_search_form">
-                <input type="hidden" name="form_token" value="<?php print drupal_get_token('maybsearch'); ?>">
+            <form action="/" method="post" class="navbar-form navbar-left" id="islandora-solr-simple-search-form" accept-charset="UTF-8">
+              <div>
+                <div class="form-group container-inline form-wrapper" id="edit-simple">
+                  <div class="form-item form-type-textfield form-item-islandora-simple-search-query">
+                    <input class="form-control navbar-search-grow" type="text" id="edit-islandora-simple-search-query" name="islandora_simple_search_query" value="" size="15" maxlength="128" class="form-text" placeholder="Search">
+                  </div>
+                </div>
               </div>
+              <input type="hidden" name="form_token" value="<?php print drupal_get_token('maybsearch'); ?>">
+              <input type="hidden" name="form_id" value="islandora_solr_simple_search_form">
             </form>
             <button type="submit" form="islandora-solr-simple-search-form" class="btn btn-default form-submit" name="op" value="search" id="edit-submit"><span class="glyphicon glyphicon-search nav-search-icon" aria-hidden="true"></span></button>
           </div>
