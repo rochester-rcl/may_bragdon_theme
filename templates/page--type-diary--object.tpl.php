2850cdbf883e69fb55c61f1ae917d3ec8bcf9d80
Adam Traub <greenlibrarian@gmail.com>
2016-02-19 10:36:12 -0500

    Adding Tab Titles
    …because empty tabs aren’t helpful.

--- templates/page--type-diary--object.tpl.php
+++ templates/page--type-diary--object.tpl.php
@@ -223,8 +223,8 @@
         </button>
         <h4 class="modal-title">Cite This</h4>
         <ul class="nav nav-tabs" role="tablist">
-          <li role="presentation" id="apaTab" class="active"><a href="#apaStyle" aria-controls="apaStyle" role="tab" data-toggle="tab"></a></li>
-          <li role="presentation" id="mlaTab"><a href="#mlaStyle" aria-controls="mlaStyle" role="tab" data-toggle="tab"></a></li>
+          <li role="presentation" id="apaTab" class="active"><a href="#apaStyle" aria-controls="apaStyle" role="tab" data-toggle="tab">APA</a></li>
+          <li role="presentation" id="mlaTab"><a href="#mlaStyle" aria-controls="mlaStyle" role="tab" data-toggle="tab">MLA</a></li>
         </ul>
       </div>
       <div class="modal-body">
