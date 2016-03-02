e5860c7d479ac35d0aff95771e35636b62bc5186
hnayak <hnayak@library.rochester.edu>
2016-03-02 13:22:55 -0500

    switch order of attributes

--- templates/page--type-diary--object.tpl.php
+++ templates/page--type-diary--object.tpl.php
@@ -228,7 +228,7 @@
 
 
 <!-- ====== Ographys ======= --->
-<div id="OgraphyModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="OgraphyModal" aria-hidden="true">
+<div class="modal fade" id="OgraphyModal" tabindex="-1" role="dialog" aria-labelledby="OgraphyModal" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
