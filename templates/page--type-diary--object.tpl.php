ff5993b352e6e3ccf4505c93058be097b1b553ae
Sean Morris <smorris@library.rochester.edu>
2016-03-08 09:53:36 -0500

    fixing modal on cite

--- templates/page--type-diary--object.tpl.php
+++ templates/page--type-diary--object.tpl.php
@@ -228,17 +228,19 @@ Test
 
 
 <!-- ====== Ographys ======= --->
-<div class="modal fade" id="OgraphyModal" tabindex="-1" role="dialog" aria-labelledby="OgraphyModal" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <!-- <h4 class="modal-title" id="myModalLabel">Modal title</h4> -->
-            </div>
-            <div class="modal-body edit-content">
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+<div class="ograpy-modal">
+    <div class="modal fade" id="OgraphyModal" tabindex="-1" role="dialog" aria-labelledby="OgraphyModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <!-- <h4 class="modal-title" id="myModalLabel">Modal title</h4> -->
+                </div>
+                <div class="modal-body edit-content">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
             </div>
         </div>
     </div>
