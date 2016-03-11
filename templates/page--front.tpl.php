8b824c0116a74f876a00d1d9ac1a7b660ed0dd29
Sean Morris <smorris@library.rochester.edu>
2016-03-11 11:29:30 -0500

    adding indicators

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -91,9 +91,11 @@
 <div id="carousel-example-generic" class="carousel slide no-mobile" data-ride="=">
   <!-- Indicators -->
   <ol class="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" class="active">    </li>
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
         <li data-target="#carousel-example-generic" data-slide-to="1"></li>
         <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="4"></li>
   </ol>
 
 
