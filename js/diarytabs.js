0621591e4f0008b49f11753756bdf31065fa1f44
Sean Morris <smorris@library.rochester.edu>
2016-03-14 14:49:26 -0400

    redo

--- js/diarytabs.js
+++ js/diarytabs.js
@@ -1,34 +1,22 @@
 (function ($) {
     $(document).ready(function(){
-        console.log('Here');
         $('input[type="radio"]').click(function(){
+            console.log("Here!")
             if($(this).attr("value")=="tei"){
-                console.log('tei');
-                $('.tei').css({
-                    'background':'red'
-                });
-                // $(".box").not("#paged-tei-seadragon-viewer-tei").hide("#paged-tei-seadragon-viewer-seadragon-pane");
-                // $("#paged-tei-seadragon-viewer-tei").show();
+                console.log("TEI")
+                $(".box").not(".red").hide();
+                $(".red").show();
             }
             if($(this).attr("value")=="viewer"){
-                console.log('viewer');
-                $('.tei').css({
-                    'background':'blue'
-                });
-                // $(".box").not(".green").hide();
-                // $(".green").show();
+                console.log("Viewer")
+                $(".box").not(".green").hide();
+                $(".green").show();
             }
             if($(this).attr("value")=="inclusions"){
-                console.log('inclusions');
-                $('.tei').css({
-                    'background':'green'
-                });
-                // $(".box").not(".blue").hide();
-                // $(".blue").show();
-            } else {
-                console.log('default');
-                $('.tei').css({
-                    'background':'red'
-                });
-            });
+                console.log("Inclusions")
+                $(".box").not(".blue").hide();
+                $(".blue").show();
+            }
+        });
+    });
 })(jQuery);
