d5fa55ec5d2fd1afa5adad6919f0daeef4893666
Sean Morris <smorris@library.rochester.edu>
2016-03-14 14:50:10 -0400

    test 2

--- js/diarytabs.js
+++ js/diarytabs.js
@@ -1,19 +1,19 @@
 (function ($) {
     $(document).ready(function(){
         $('input[type="radio"]').click(function(){
-            console.log("Here!")
+            console.log("Here!");
             if($(this).attr("value")=="tei"){
                 console.log("TEI")
                 $(".box").not(".red").hide();
                 $(".red").show();
             }
             if($(this).attr("value")=="viewer"){
-                console.log("Viewer")
+                console.log("Viewer");
                 $(".box").not(".green").hide();
                 $(".green").show();
             }
             if($(this).attr("value")=="inclusions"){
-                console.log("Inclusions")
+                console.log("Inclusions");
                 $(".box").not(".blue").hide();
                 $(".blue").show();
             }
