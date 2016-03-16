dcdd425d06b7cda6f91262c6c1bc01725a9de784
Sean Morris <smorris@library.rochester.edu>
2016-03-16 09:07:44 -0400

    changed id to the icon

--- js/teitoggle.js
+++ js/teitoggle.js
@@ -10,7 +10,7 @@
     $('#yourButton').click(function() {
         if ($('br').css('height') === "17px"){
             console.log("If this...");
-            $('#yourButton').toggleClass('.fa-align-justify');
+            $('#texticon').toggleClass('.fa-align-justify');
             $('br').css({
                 'content': 'normal',
                 'margin-bottom': '0',
@@ -21,7 +21,7 @@
             });
         } else {
             console.log("Else do this...");
-            $('#yourButton').toggleClass('.fa-align-left');
+            $('#texticon').toggleClass('.fa-align-left');
             $('br').css({
                 'content': '"A"',
                 'margin-bottom': '1.5em !important',
