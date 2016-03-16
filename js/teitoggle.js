94c9b71e29edf39370f88c61e210a5e494d07a96
Sean Morris <smorris@library.rochester.edu>
2016-03-16 11:47:22 -0400

    Had the wrong ID

--- js/teitoggle.js
+++ js/teitoggle.js
@@ -12,7 +12,7 @@
             console.log("If this...");
             $('#texticon').removeClass("fa-align-left");
             $('#texticon').addClass("fa-align-justify");
-            $('#texticon').prop('title', 'your new title');
+            $('#yourButton').prop('title', 'your new title');
             $('br').css({
                 // 'content': 'normal',
                 // 'margin-bottom': '0',
