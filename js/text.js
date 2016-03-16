a09f76143ecf8acd312dfec6aea8d17910e9c7a9
Sean Morris <smorris@library.rochester.edu>
2016-03-16 14:29:14 -0400

    fixed syntax error

--- js/text.js
+++ js/text.js
@@ -1,3 +1,4 @@
 (function ($) {
-        console.log("I am also here, but not looking at you!");        
+        console.log("I am also here, but not looking at you!");
+        $('persname').html($('persname').html().replace("(","<span>").replace(")","</span>"));
 })(jQuery);
