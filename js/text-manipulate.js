2f5f1ee3606e67631f0f84709793ae44e16d86b2
Sean Morris <smorris@library.rochester.edu>
2016-03-16 14:14:16 -0400

    adding <a> selector

--- js/text-manipulate.js
+++ js/text-manipulate.js
@@ -2,6 +2,6 @@
 
 
 (function ($) {
-    $('#paged-tei-seadragon-viewer-download-datastream-JP2').html($('#paged-tei-seadragon-viewer-download-datastream-JP2').html().replace("(","<span>").replace(")","</span>"));
+    $('#paged-tei-seadragon-viewer-download-datastream-JP2 a').html($('#paged-tei-seadragon-viewer-download-datastream-JP2 a').html().replace("(","<span>").replace(")","</span>"));
 });
 })(jQuery);
