ec063191a1728cdc15764ce7e1aa8a5501ba4ab5
Sean Morris <smorris@library.rochester.edu>
2016-03-16 14:17:58 -0400

    renaming

--- js/text-manipulate.js
+++ js/text-manipulate.js
@@ -2,7 +2,7 @@
 
 
 (function ($) {
-    console.log("I am also here, but not looking at you!");
-    $('#paged-tei-seadragon-viewer-download-datastream-JP2 a').html($('#paged-tei-seadragon-viewer-download-datastream-JP2 a').html().replace("(","<span>").replace(")","</span>"));
-});
+        console.log("I am also here, but not looking at you!");
+        $('#paged-tei-seadragon-viewer-download-datastream-JP2').html($('#paged-tei-seadragon-viewer-download-datastream-JP2').html().replace("(","<span>").replace(")","</span>"));
+    });
 })(jQuery);
