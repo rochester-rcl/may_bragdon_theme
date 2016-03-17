5b8af55b0b48e35ce5d5784564b431a150597f90
Sean Morris <smorris@library.rochester.edu>
2016-03-17 09:31:30 -0400

    update js, popover fixed pos

--- js/text.js
+++ js/text.js
@@ -1,4 +1,4 @@
 (function ($) {
         console.log("I am also here, but not looking at you!");
-        $('#target').html($('#target').html().replace("(","<span>").replace(")","</span>"));
+        $('#paged-tei-seadragon-viewer-download-datastream-JP2').html($('#paged-tei-seadragon-viewer-download-datastream-JP2').html().replace("(","<span>").replace(")","</span>"));
 })(jQuery);
