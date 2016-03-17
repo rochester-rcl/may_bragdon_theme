ab33c6caf5e4d663b9df1b546b38ffa7faf49092
Sean Morris <smorris@library.rochester.edu>
2016-03-17 09:37:58 -0400

    JS cleanup

--- js/text.js
+++ js/text.js
@@ -1,5 +1,5 @@
 (function ($) {
         console.log("I am also here, but not looking at you!");
-        $('#paged-tei-seadragon-viewer-download-datastream-JP2').html($('#paged-tei-seadragon-viewer-download-datastream-JP2').html().replace("(","<span>").replace(")","</span>"));
+        // $('#paged-tei-seadragon-viewer-download-datastream-JP2').html($('#paged-tei-seadragon-viewer-download-datastream-JP2').html().replace("(","<span>").replace(")","</span>"));
         $('.modal-content').html($('.modal-content').html().replace("(","<span>").replace(")","</span>"));
 })(jQuery);
