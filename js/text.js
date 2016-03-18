ab60444cd3d723b46af57cfb92a375922eba1ce0
Sean Morris <smorris@library.rochester.edu>
2016-03-18 13:01:15 -0400

    Removed landing page, cleared up JS error on text.js

--- js/text.js
+++ js/text.js
@@ -1,5 +1,4 @@
 (function ($) {
         // console.log("I am also here, but not looking at you!");
         $('#paged-tei-seadragon-viewer-download-datastream-JP2').html($('#paged-tei-seadragon-viewer-download-datastream-JP2').html().replace("(","<span>").replace(")","</span>"));
-        $('persname').html($('persname').html().replace("(","<span>").replace(")","</span>"));
 })(jQuery);
