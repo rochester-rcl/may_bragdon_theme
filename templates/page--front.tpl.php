02b2334af42dc194d69bb8392aac56e545d83e86
Sean Morris <smorris@library.rochester.edu>
2016-04-07 12:13:26 -0400

    Scope - added placeholders for all dates

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -271,6 +271,7 @@
     <img class="box-images" alt="Diary April 6, 1895 to February 26, 1896" src="<?php print base_path() . drupal_get_path('theme', 'may_bragdon_theme');?>/images/1896.jpg" />
     <div class="box-text">
       <div class="box-date">April 6, 1895 - February 26, 1896</div>
+      <div class="box-page-count">308 Pages, 53 inclusions</div>
     </div>
   </a>
 </div>
@@ -281,6 +282,7 @@
     <img class="box-images" alt="Diary February 29, 1896 to February 24, 1897" src="<?php print base_path() . drupal_get_path('theme', 'may_bragdon_theme');?>/images/1895-96.jpg" />
     <div class="box-text">
       <div class="box-date">February 29, 1896 - February 24, 1897</div>
+      <div class="box-page-count">294 Pages, 70 inclusions</div>
     </div>
   </a>
 </div>
@@ -289,9 +291,8 @@
   <a href="https://rclind2.lib.rochester.edu/maybragdon/islandora/object/maybragdon%3A3356">
     <img class="box-images" alt="Diary February 25, 1897 to November 11, 1898" src="<?php print base_path() . drupal_get_path('theme', 'may_bragdon_theme');?>/images/1897-98.jpg" />
     <div class="box-text">
-      <!-- <div class="box-headline">1897-1898</div> -->
       <div class="box-date">February 25, 1897 - November 11, 1898</div>
-
+      <div class="box-page-count">308 Pages, 53 inclusions</div>
     </div>
   </a>
 </div>
@@ -302,7 +303,7 @@
     <div class="box-text">
       <!-- <div class="box-headline">1897-1898</div> -->
       <div class="box-date">November 12, 1898 - January 18, 1902</div>
-
+      <div class="box-page-count">308 Pages, 53 inclusions</div>
     </div>
   </a>
 </div>
@@ -311,8 +312,8 @@
   <a href="https://rclind2.lib.rochester.edu/maybragdon/islandora/object/maybragdon%3A7854">
     <img class="box-images" alt="Diary January 19, 1902 to February 4, 1904" src="<?php print base_path() . drupal_get_path('theme', 'may_bragdon_theme');?>/images/1902-04.jpg" />
     <div class="box-text">
-      <!-- <div class="box-headline">1897-1898</div> -->
       <div class="box-date">January 19, 1902 - February 4, 1904</div>
+      <div class="box-page-count">308 Pages, 53 inclusions</div>
     </div>
   </a>
 </div>
@@ -321,8 +322,8 @@
   <a href="https://rclind2.lib.rochester.edu/maybragdon/islandora/object/maybragdon%3A9149">
     <img class="box-images" alt="Diary February 26, 1904 to June 8, 1905" src="<?php print base_path() . drupal_get_path('theme', 'may_bragdon_theme');?>/images/1904-05.jpg" />
     <div class="box-text">
-      <!-- <div class="box-headline">1897-1898</div> -->
       <div class="box-date">February 26, 1904 - June 8, 1905</div>
+      <div class="box-page-count">308 Pages, 53 inclusions</div>
     </div>
   </a>
 </div>
@@ -331,8 +332,8 @@
   <a href="https://rclind2.lib.rochester.edu/maybragdon/islandora/object/maybragdon%3A12692">
     <img class="box-images" alt="Diary June 9, 1905 to October 17, 1907" src="<?php print base_path() . drupal_get_path('theme', 'may_bragdon_theme');?>/images/1905-07.jpg" />
     <div class="box-text">
-      <!-- <div class="box-headline">1897-1898</div> -->
       <div class="box-date">June 9, 1905 - October 17, 1907</div>
+      <div class="box-page-count">308 Pages, 53 inclusions</div>
     </div>
   </a>
 </div>
@@ -341,8 +342,8 @@
   <a href="https://rclind2.lib.rochester.edu/maybragdon/islandora/object/maybragdon%3A14209">
     <img class="box-images" alt="Diary October 18, 1907 to December 31, 1909" src="<?php print base_path() . drupal_get_path('theme', 'may_bragdon_theme');?>/images/1907-09.jpg" />
     <div class="box-text">
-      <!-- <div class="box-headline">1897-1898</div> -->
         <div class="box-date">October 18, 1907 - December 31, 1909</div>
+        <div class="box-page-count">308 Pages, 53 inclusions</div>
     </div>
   </a>
 </div>
@@ -351,8 +352,8 @@
   <a href="https://rclind2.lib.rochester.edu/maybragdon/islandora/object/maybragdon%3A16177">
     <img class="box-images" alt="Diary January 1, 1910 to December 31, 1914" src="<?php print base_path() . drupal_get_path('theme', 'may_bragdon_theme');?>/images/1910-14.jpg" />
     <div class="box-text">
-      <!-- <div class="box-headline">1897-1898</div> -->
       <div class="box-date">January 1, 1910 - December 31, 1914</div>
+      <div class="box-page-count">308 Pages, 53 inclusions</div>
     </div>
   </a>
 </div>
