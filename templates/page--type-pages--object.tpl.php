f1fc51f1e0da9c14705ebf284a324eef51c34d3f
Sean Morris <smorris@library.rochester.edu>
2016-04-08 09:08:16 -0400

    Page - back btn remove old position

--- templates/page--type-pages--object.tpl.php
+++ templates/page--type-pages--object.tpl.php
@@ -211,9 +211,7 @@
       <?php endif; ?>
 
       <div class="content-h-c">
-        <div class="back-to-diary-btn">
-            <a href="<?php print base_path() ?>islandora/object/<?php print $pid ?>">Back To Diary</a>
-        </div>
+
         <?php print render($page['content']); ?>
       </div>
 
