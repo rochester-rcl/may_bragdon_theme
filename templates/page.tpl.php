e14fad278ed224512f7c9fcc1cd795e8eafa7499
Adam Traub <greenlibrarian@gmail.com>
2016-04-15 09:25:56 -0400

    Changing Additions back to Inclusions

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -134,7 +134,7 @@
                    </li>
                    </ul>
                    <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Additions<span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Inclusions<span class="caret"></span></a>
                     <ul class="dropdown-menu">
                       <li><a href="/maybragdon/islandora/search/mods_genre_mt%3A%28clippings%29">Clippings</a></li>
                       <li><a href="/maybragdon/islandora/search/mods_genre_mt%3A%28correspondence%29">Correspondence</a></li>
