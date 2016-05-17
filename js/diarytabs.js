086aeb88672aff023d9b70b94576eff6a300845c
Sean Morris <smorris@library.rochester.edu>
2016-05-17 12:37:41 -0400

    mobile tweak

--- js/diarytabs.js
+++ js/diarytabs.js
@@ -1,5 +1,6 @@
 (function ($) {
     $(document).ready(function(){
+        $('#paged-tei-seadragon-viewer-tei').removeClass("hide");
         // console.log("I am here and watching!");
         $('input[type="radio"]').click(function(){
             if($(this).attr("value")=="tei"){
